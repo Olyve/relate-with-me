717f3adfd76569f56e39c5ed13a7fc859f96d2c6
Sam Galizia <samgalizia@gmail.com>
2018-09-29 16:17:50 -0700

    Rename Public directory to Docs
    Renamed the public directory and changed it to `docs` as that makes more
    sense as to what is actually being hosted.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Relate With Me API
+
+Welcome to the Relate With Me API! In this project you will find documentation that is also hosted from the route `/docs` once the server is hosted. Any questions that may need to be answered should be explained in the Docs.
--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /** Configure docs directory */
-app.use('/docs', express.static(path.join(__dirname, '/public')));
+app.use('/docs', express.static(path.join(__dirname, '/docs')));
 
 /** Configure the routes */
 app.use('/', indexRouter);
